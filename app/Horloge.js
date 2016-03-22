715a32fadb1b0683f8fe7d28974fdd26fd826ff3
Margoot <lerouzic.margot@gmail.com>
2016-03-22 22:14:32 +0100

    Horloge avec modif

--- app/Horloge.js
+++ app/Horloge.js
@@ -1,11 +1,12 @@
 /**
  * Created by simonthome on 18/03/2016.
  */
+
 'use strict';
 
 const EventEmitter = require('events').EventEmitter;
 const ev = new EventEmitter();
-const interv  = setInterval(() => ev.emit('hour', i++ % 24), 1000);
+const interv  = setInterval(() => ev.emit('hour', i++ % 24), 100);
 let i = 0;
 ev.on('hour', (hour) => console.log(`il est ${hour}`));
 
