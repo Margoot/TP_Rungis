110beeb73f92bad03461096285a00d8552a2c2f7
jeremiebillot <jeremie.billot@gmail.com>
2016-03-23 00:04:03 +0100

    class client

--- app/Client.js
+++ app/Client.js
@@ -1,3 +1,18 @@
-/**
- * Created by simonthome on 18/03/2016.
- */
+'use strict'
+
+const Promise = require('bluebird');
+
+//definition de variable
+const MIN_TO_SEC = 100;
+const HOUR_TO_SEC = 10000;
+
+
+class Client {
+    constructor(clientNumber, hunger, resistance) {
+        this.clientNumber = 1;
+        this.hunger = true;
+        this.resistance = Match.floor(Math.random() * (4000) + 1000);
+
+
+    }
+}
