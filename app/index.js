e69c6d5fe122d4e073c801957a307577f5d4570e
jeremiebillot <jeremie.billot@gmail.com>
2016-03-22 22:45:03 +0100

    Class client

--- app/index.js
+++ app/index.js
@@ -1,6 +1,21 @@
-/**
- * Created by simonthome on 18/03/2016.
- */
-'use strict';
+'use strict'
 
-console.log('jkllk');
+const Promise = require('bluebird');
+
+//definition de variable
+var tpsRes= 3001;
+var tpsResMin = 1000;
+var Client = [];
+
+
+class Client{
+    constructor(numClient, faim, resistance){
+        this.numClient = 1;
+        this.faim = true;
+        this.resistance = Math.random() * (tpsRes) + tpsResMin;
+
+        this.addClient = function(numClient, faim, resistance){
+            this.Client.push(new Personne(numClient, faim, resistance));
+        };
+
+    }
