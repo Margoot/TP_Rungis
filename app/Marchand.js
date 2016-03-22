39ee3afbaf2b735e96e0f9ffe9edaf5735716db9
Margoot <lerouzic.margot@gmail.com>
2016-03-22 22:28:20 +0100

    avec bool

--- app/Marchand.js
+++ app/Marchand.js
@@ -13,7 +13,8 @@ class Marchand {
     constructor (){
         this.openTime = 5*HOUR_TO_SEC;
         this.closeTime = 14*HOUR_TO_SEC;
-        this.refuelingTime = Math.floor((Math.random() * (HOUR_TO_SEC + 15*MIN_TO_SEC)) + 15 * MIN_TO_SEC)
+        this.refuelingTime = Math.floor((Math.random() * (HOUR_TO_SEC + 15*MIN_TO_SEC)) + 15 * MIN_TO_SEC);
+        this.open = false;
         this.stock = ["Bacon","Pasta","Creme","Eggs","Onions","Salad","Tomatoes","Mozarella","Chicken","Parmesan"];
     }
 
