ac7b6d23950f092c52ca029d90090738611daa21
simonthme <thome.simon@gmail.com>
2016-03-25 16:55:04 +0100

    Rungis
    temps aléatoire ouverture puis ravitaillement, temps de ravitaillement,
    fin ravitaillement, fermeture du marché

--- app/Marchand.js
+++ app/Marchand.js
@@ -6,21 +6,71 @@
 const Promise = require('bluebird');
 const winston = require('winston');
 
-const MIN_TO_SEC = 100;
-const HOUR_TO_SEC = 10000;
+const HOUR_TO_MIN = 100;
 
 class Marchand {
     constructor (){
-        this.openTime = 5*HOUR_TO_SEC;
-        this.closeTime = 14*HOUR_TO_SEC;
-        this.refuelingTime = Math.floor((Math.random() * (HOUR_TO_SEC + 15*MIN_TO_SEC)) + 15 * MIN_TO_SEC);
-        this.open = false;
+        this.openTime = 5*HOUR_TO_MIN;
+        this.closeTime = 14*HOUR_TO_MIN;
+        this.refuelingTime = Math.floor((Math.random() * (HOUR_TO_MIN + 15)) + 15);
+        //this.open = false;
         this.stock = ["Bacon","Pasta","Creme","Eggs","Onions","Salad","Tomatoes","Mozarella","Chicken","Parmesan"];
     }
 
+    opening () {
+        var open = new Promise ((resolve,reject)=>{
+            setTimeout(() => {console.log("le marché est ouvert");
+                resolve(true)}, 5000);
 
+
+
+        });
+
+        open
+            .then(() => {this.refueling();
+            })
+            .catch(err => console.log(`Error : ${err}`));
+
+
+    }
+
+    closing (open) {
+
+        if (open){
+            var close = new Promise ((resolve,reject)=>{
+                setTimeout(() => resolve(true), 5000);
+
+            });
+        }
+
+        close
+            .then(() => {console.log("le marché est fermé")})
+            .catch(err => console.log(`Error : ${err}`));
+
+
+
+    };
+
+    refueling() {
+      var refuel = new Promise ((resolve, reject) => {
+
+          console.log("Début du ravitaillement");
+          setTimeout(() => {console.log("fin du ravitaillement");
+              resolve(true)},10000);
+      });
+
+        refuel
+            .then((end) => {this.closing(end);
+                })
+            .catch(err => console.log(`Error : ${err}`));
+    };
 
 
 
 
 }
+
+var marche = new Marchand();
+marche.opening();
+//
+//marche.refueling();
